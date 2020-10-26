37ca29f97ccaa0528961f2052a652f500f0b6d15
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-26 20:33:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// (1)Use bracket notation to find the last character in the lastName variable.
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// (2) Use bracket notation to find the second-to-last character in the lastName string.
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
